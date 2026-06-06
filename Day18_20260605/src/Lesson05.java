40fb0b7ed646e948d9a5df65c3c032773f06c3d7
unuf0517 <2015150582@qq.com>
2026-06-06 19:46:43 +0800

    字符流，逻辑和字节流差不多

--- Day18_20260605/src/Lesson05.java
+++ Day18_20260605/src/Lesson05.java
@@ -1,8 +1,20 @@
 import java.io.FileReader;
+import java.io.FileWriter;
 
 public class Lesson05 {
     public static void main(String[] args) throws Exception {
         FileReader fr=new FileReader("E:\\class\\code\\Day18_20260605\\text.txt");
         System.out.println((char)fr.read());
+
+        char[] chars=new char[4];
+        int len=fr.read(chars);
+        System.out.println(new String(chars, 0, len));
+
+        FileWriter fw=new FileWriter("E:\\class\\code\\Day18_20260605\\output.txt");
+        fw.write('A');
+        char[] chars2={'B', 'C', 'D', 'E'};
+        fw.write(chars2);
+        fw.flush();//刷新流，强制将缓冲区中的数据写入文件
+        fw.close();
     }
 }
